b621b5b9017cf9c366f2b195bba8d4d1690eedae
Sam Kauffman <samuelkauffman@gmail.com>
2015-03-11 16:26:05 -0400

    added order route

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,20 @@
 'use strict';
 
 angular.module('skedaddle', ['ngAnimate', 'ngCookies', 'ngSanitize', 'restangular', 'ui.router'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/main/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .state('order', {
+                url: '/',
+                templateUrl: 'app/order/order.html',
+                contrller: 'OrderCtrl',
+                controllerAs: 'order'
+            });
 
-    $urlRouterProvider.otherwise('/');
-  })
-;
+        $urlRouterProvider.otherwise('/');
+    });
